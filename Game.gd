af497657f87a35c1e04f1136663eea7b0fe10920
AlexABallWebDev <alexball@alexball.me>
2019-09-29 11:35:08 -0700

    Add hex generation logic and sample json hex grid for testing. Add hex with x in center to represent end points for the game.

--- Game.gd
+++ Game.gd
@@ -1,1 +1,67 @@
 extends Node2D
+
+var hexes
+var hex_scene
+
+const SCREEN_OFFSET = 100
+const HEX_WIDTH = 128
+const HEX_HEIGHT = 128
+
+# define where hexes spawn from json
+var hex_json = """
+{
+	"hex_rows": [
+		["x", "n", "o", "x"],
+		["o", "o", "o", "n"]
+	]
+}
+"""
+
+func _ready():
+	print("parsing json")
+	var json_result = JSON.parse(hex_json)
+	print(json_result.error)
+	print(json_result.result)
+	
+	hex_scene = load("res://Hex.tscn")
+	
+	_generate_hexes(json_result.result)
+
+# generate hexes and store them
+func _generate_hexes(hex_data):
+	# hex grid knowledge learned from
+	# https://www.redblobgames.com/grids/hexagons/
+	hexes = []
+
+	var row_count = -1
+	for row in hex_data.hex_rows:
+		row_count += 1
+
+		hexes.push_back([])
+		print("row " + str(row_count))
+
+		var cell_count = -1
+		for hex_char in row:
+			cell_count += 1
+			print("cell " + str(cell_count))
+			if hex_char == "x" or hex_char == "o":
+				var node = hex_scene.instance()
+
+				# Move the new hex into its spot on the grid
+				var hex_x = cell_count * HEX_WIDTH + SCREEN_OFFSET
+				var hex_y = row_count * HEX_HEIGHT / 4 * 3 + SCREEN_OFFSET
+				# offset for shoving odd rows +1/2 a column
+				if row_count % 2 == 1:
+					hex_x += HEX_WIDTH / 2
+				node.position = Vector2(hex_x, hex_y)
+				add_child(node)
+				print(str(row_count) + " " + str(node))
+				hexes[row_count].push_back(node)
+				
+				if hex_char == "x":
+					hexes[row_count][cell_count].change_color(Color.red)
+					hexes[row_count][cell_count].set_x()
+					
+			else:
+				hexes[row_count].push_back(null)
+	print(str(hexes))
--- Hex.gd
+++ Hex.gd
@@ -4,12 +4,14 @@ extends Node2D
 var color
 
 var hex_dot_image
+var hex_x_image
 
 var global
 
 func _ready():
 	global = $"/root/Global"
 	hex_dot_image = load("res://hex_dot.png")
+	hex_x_image = load("res://hex_x.png")
 
 # make hex clickable
 func _on_Area2D_input_event(_viewport, event, _shape_idx):
@@ -23,3 +25,9 @@ func _on_click():
 	color = global.cursor_color
 	$Sprite.texture = hex_dot_image
 	$Sprite.modulate = color
+	
+func change_color(new_color):
+	$Sprite.modulate = new_color
+
+func set_x():
+	$Sprite.texture = hex_x_image
